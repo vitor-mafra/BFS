f3c063404c03d1997f01be05556af8dbb17011f0
Vitor  Mafra <vitor.mafra@dcc.ufmg.br>
2020-09-07 16:59:30 -0300

    Add files via upload

---
+++ include/CasaTabuleiro.h
@@ -0,0 +1,24 @@
+#ifndef CASATABULEIRO_H
+#define CASATABULEIRO_H
+
+#include <vector>
+
+class CasaTabuleiro{
+    public:
+    int x, y; // Posicao da casa no tabuleiro
+    int numPulos;
+    int puloAnterior;
+    bool casaFinal; // Limite do tabuleiro e objetivo do jogo
+    bool ligadaAFinal;
+    char jogadorPresente;
+    // vetor com todas as ligacoes que aquela casa faz
+    std::vector<CasaTabuleiro*> casasLigadas;
+
+    // Construtor
+    CasaTabuleiro(int x, int y, int numPulos, bool casaFinal, char jogadorPresente);
+
+    void adicionaLigacao(CasaTabuleiro* casa);
+};
+
+
+#endif // CASATABULEIRO_H
